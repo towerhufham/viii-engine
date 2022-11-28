1806d8ad100121ef800f7d6c54f8f67cbc348a04
towerhufham <32599109+towerhufham@users.noreply.github.com>
2022-11-27 20:17:55 -0500

    Squall OUR BOY

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -7,7 +7,7 @@ export class AppOne {
         this.engine = new BABYLON.Engine(canvas)
         window.addEventListener('resize', () => {
             this.engine.resize();
-        });
+        })
         this.scene = createScene(this.engine, this.canvas)
 
     }
@@ -24,41 +24,48 @@ export class AppOne {
         this.debug(true);
         this.engine.runRenderLoop(() => {
             this.scene.render();
-        });
+        })
     }
 
 }
 
 
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
+const createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     // this is the default code from the playground:
 
     // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+    const scene = new BABYLON.Scene(engine)
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
 
     // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.setTarget(BABYLON.Vector3.Zero())
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    light.intensity = 0.7
 
     // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+    // const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene)
 
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+    // sphere.position.y = 1
+
+    const sm = new BABYLON.SpriteManager("Squall Manager", "src/img/squall-idle.png", 1, {width: 61, height: 77}, scene)
+    const squall = new BABYLON.Sprite("Squall", sm)
+    squall.width = (61 * 3) / 100
+    squall.height = (77 * 3) / 100
+    squall.position.y = 1
+    squall.playAnimation(0, 3, true, 150)
 
     // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene)
 
-    return scene;
+    return scene
 };
